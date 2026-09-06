46d5f07ca9b44cc87ba895c387ffc16f0d98eeef
riskiekalimuda <riskie.kalimuda@gmail.com>
2026-09-06 22:44:41 +0700

    add OrderResultMessage

---
+++ MQ/Messages/OrderMsv/OrderResultMessage.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MessageMQCommon.MQ.Messages.OrderMsv
+{
+    public class OrderResultMessage
+    {
+        public string OrderNumber { get; set; } = string.Empty; 
+        public string OrderResult { get; set; } = string.Empty; 
+    }
+}
