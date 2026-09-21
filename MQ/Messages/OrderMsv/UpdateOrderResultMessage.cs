d81b013321a806eccaa837d12777b50b0af19b7d
riskiekalimuda <riskie.kalimuda@gmail.com>
2026-09-21 12:14:13 +0700

    add update order message and queue names

---
+++ MQ/Messages/OrderMsv/UpdateOrderResultMessage.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MessageMQCommon.MQ.Messages.OrderMsv
+{
+    public class UpdateOrderResultMessage
+    {
+        public string OrderNumber { get; set; } = string.Empty; 
+        public string UpdateOrderResult { get; set; } = string.Empty; 
+    }
+}
--- MQ/Names/QueueNames.cs
+++ MQ/Names/QueueNames.cs
@@ -12,6 +12,7 @@ namespace MessageMQCommon.MQ.Names
             public static readonly string AddOrderQueue = "add-order-queue";
             public static readonly string AddOrderResultQueue = "add-order-result-queue";
             public static readonly string UpdateOrderQueue = "update-order-queue";
+            public static readonly string UpdateOrderResultQueue = "update-order-result-queue";
         }  
         public static class PurchaseQueue
         {
